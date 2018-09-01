fd3a7c9c5762867290bc1a70ec06bf9e19561530
nryedida <nryedida@gmail.com>
2018-09-01 08:52:20 +0530

    first cut
    extracting the last two words from a string and grep against trigram -
    TODO -
    -should return the string instead of daata frame subset
    -should have logic to always return 3 words - currently if trigrams do not match it returns empty data frame

---
+++ predictR.R
@@ -0,0 +1,40 @@
+#script that will load the n-gram file(s) into data table and provides a function that can predict the next word given a string
+
+#read each of the n-gram file in to an n-gram data frame
+
+predictNextWord <- function(s) {
+    #assume we only have trigrams
+    #get the number of words entered
+    #if empty string stop
+    #if length is 1, concatenate with _ and search in bigrams
+    #if length is 2,  concatenate with _ and search in trigrams
+    #if length > 2 take the last two and search in trigrams 
+    
+    # print(paste("entered string = ", s))
+    sl <- strsplit(s," ")
+    v <- unlist(sl)
+    len <- length(v)
+    if(len < 1) stop("Please enter at least a word")
+    # if (len == 1) print(paste("word entered = ", v[1]))
+    # if (len == 2) {
+    #         print("wordss entered are = ")
+    #         print(v)
+    # }
+    # for (i in len:1) {
+    #     if (i>1) print(paste(v[i-1], v[i], sep="_"))
+    # }
+    toMatch <- paste(v[len-1], v[len], sep="_")
+    print(paste0("toMatch is ", toMatch))
+    # print(sv)
+    # print(paste("length(sv) = ", length(sv)))
+    # print(paste("sv = ", sv))
+    # length(strsplit(s," ", fixed = TRUE))
+    stm <- paste(toMatch,collapse="|")
+    print(paste0("stm before is ", stm))
+    stm <- paste0("^", paste0(stm,"_"))
+    print(paste0("stm after is ", stm))
+    print(head(trifreq[grep(stm,trifreq$feature),1],3))
+}
+
+# predictNextWord("how are you")
+predictNextWord("how are to the")
