81f0086fd71317b37d498b4aab4a40f30bb98854
nryedida <nryedida@gmail.com>
2018-09-09 10:41:49 +0530

    handles all grams upto 5 - uses data table indexing

--- testActionButton/server.R
+++ testActionButton/server.R
@@ -1,68 +1,134 @@
 library(shiny)
+library(stringi)
+library(data.table)
 
 initializeSession <- function () {
-    if (file.exists("bigram.Rds")) {
-        bifreq <<- readRDS("bigram.Rds")
+    if (file.exists("unigrams.Rds")) {
+        unifreq <<- readRDS("unigrams.Rds")
+        setkey(unifreq, feature)
     } else {
         cat("File not found")
     }
-    if (file.exists("trigram.Rds")) {
-        trifreq <<- readRDS("trigram.Rds")
+    if (file.exists("bigrams.Rds")) {
+        bifreq <<- readRDS("bigrams.Rds")
+        setkey(bifreq, feature)
+    } else {
+        cat("File not found")
+    }
+    if (file.exists("trigrams.Rds")) {
+        trifreq <<- readRDS("trigrams.Rds")
+        setkey(trifreq, feature)
+    } else {
+        cat("File not found")
+    }
+    if (file.exists("quadgrams.Rds")) {
+        quadfreq <<- readRDS("quadgrams.Rds")
+        setkey(quadfreq, feature)
+    } else {
+        cat("File not found")
+    }
+    if (file.exists("pentagrams.Rds")) {
+        pentafreq <<- readRDS("pentagrams.Rds")
+        setkey(pentafreq, feature)
     } else {
         cat("File not found")
     }
 }
 
-# predictWords <- function(s) {
-#     #assume we only have trigrams
-#     #get the number of words entered
-#     #if empty string stop
-# 
-#     #call predictNextWord till we have 3 words
-#     while (length(pred) < 3) {
-#         #call predict function with string, ngram(uni, bi or tri), and number of words to predict
-#         #predictNextWords(string, model, n)        
-#     }
-#     #if length is 1, concatenate with _ and search in bigrams
-#     #if length is 2,  concatenate with _ and search in trigrams
-#     #if length > 2 take the last two and search in trigrams 
-# }
-
-predictNextWord <- function(s) {
+predictWords <- function(s)
+# predict.baseline <- function(s)
+{
+    gramSize <- 5
+    s <- stri_replace_all_regex(s, "[^[:alnum:][:space:]\'\\\\?]", " ")
+    #split given phrase and get vector(v) of strings
     sl <- strsplit(s," ")
     v <- unlist(sl)
-    len <- length(v)
-    if(len < 1) stop("Please enter at least a word")
-    # if (len == 1) print(paste("word entered = ", v[1]))
-    # if (len == 2) {
-    #         print("wordss entered are = ")
-    #         print(v)
-    # }
-    # for (i in len:1) {
-    #     if (i>1) print(paste(v[i-1], v[i], sep="_"))
-    # }
-    toMatch <- paste(v[len-1], v[len], sep="_")
-    # print(paste0("toMatch is ", toMatch))
-    stm <- paste(toMatch,collapse="|")
-    stm <- paste0("^", paste0(stm,"_"))
+    l <- length(v)
+    predicted <- vector("character", length = 0)
+    if (l > 0) {
+        if (l  <  gramSize) 
+            gramSize <- l+1
+        
+        nWords <- vector("character", length = 0)
+        nPredicted <- 0
+        toMatch <- NULL
+        repeat {
+            #v[l-(gramSize-1)+1] to v[l]
+            # cat("gram size", gramSize)
+            for (i in (l-(gramSize-1)+1):l) {
+                toMatch <- paste(toMatch, paste(v[i], "_", sep = ""), sep="")
+                # cat("toMatch = ", toMatch)
+            }
+            nWords <- predictNextWords(toMatch, gramSize)
+            #     #add these words to predicted to the end
+            if (length(nWords) > 0) {
+                for (i in 1:length(nWords)){
+                    if (nPredicted < 3) {
+                        predicted[nPredicted+1] <- nWords[i]
+                        nPredicted <- nPredicted+1
+                    }
+                }
+            }
+            nWords <- NULL
+            # predicted <- c(predicted, nWords)
+            if (length(predicted) >= 3){
+                # cat("breaking as predicted length>3")
+                break;
+            }
+            else {
+                gramSize <- gramSize - 1
+                toMatch <- NULL
+                # predicted <- c("a", "b", "c")
+            }
+        }
+    }
+    predicted
+}
+
+predictNextWords <- function(txtTogrep, gramSize) 
+{
+    stm <- paste(txtTogrep,collapse="|")
+    stm <- paste0("^", stm)
     # print(paste0("searching trigrams for ", stm))
-    result <- head(trifreq[grep(stm,trifreq$feature),1],3)
-    # print(paste0("class of result = ", result))
-    # s <- result
+    
+    if (gramSize == 5) {
+        result <- head(pentafreq[grep(stm,pentafreq$feature),][order(-frequency)],3)
+        result <- result[[1]]
+    } 
+    else if (gramSize == 4) {
+        result <- head(quadfreq[grep(stm,quadfreq$feature),][order(-frequency)],3)
+        result <- result[[1]]
+    } 
+    else if (gramSize == 3) {
+        result <- head(trifreq[grep(stm,trifreq$feature),][order(-frequency)],3)
+        result <- result[[1]]
+    } 
+    else if (gramSize == 2) {
+        result <- head(bifreq[grep(stm,bifreq$feature),][order(-frequency)],3)
+        result <- result[[1]]
+    } 
+    else
+    {
+        result <- head(unifreq[,1],3)[[1]]
+        # cat("from unigrams")
+        # if len(prdct) < 3, add the delta from unigram
+    }
+    
     pred <- vector(length=0)
+    # cat("class of result = ", class(result))
+    # print(paste0("class of result = ", result))
     if (length(result) > 0) {
+        # if (length(result) > 0) {
         for (i in 1:length(result)) {
             sl <- strsplit(result[i],"_")
             v <- unlist(sl)
             pred[i] <- v[length(v)]
         }
-        cat("num predictions", length(pred), sep = "\n")
-        # pred <- cat(pred, sep = "\n")
+        # if (gramSize == 3) cat("from tri grams")
+        # if (gramSize == 2) cat("from bi grams")
     }
+    # cat("\nin predicNextWords, predicted = ", pred)
     pred
-    # else {
-    #     cat("No results", sep = "\n")
-    # }
 }
 
 server <- function(input, output) {
@@ -70,11 +136,13 @@ server <- function(input, output) {
     initializeSession()
     
     x <- eventReactive(input$go, {
-        tolower(input$caption)
+        input$caption
     })
     
     output$nextword <- renderPrint({
-        cat(predictNextWord(x()), sep = "\n")
+        # ptm <- proc.time()
+        cat(predictWords(x()), sep = "\n")
+        # print(proc.time()-ptm)
         # paste0(x(), dim(trifreq))
     })
 }
